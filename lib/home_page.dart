b50cd8fb739a7f1a1ccd8b001214565b0abf9850
mikenson <mikensonthomas2@gmail.com>
2022-08-31 10:57:12 -0300

    aplicativo de celular

---
+++ lib/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Aplicativo telefonico'),
+      ),
+      body: Center(
+        child: GestureDetector(
+          child: Text(
+            'Count: $counter',
+            style: TextStyle(fontSize: 20),
+          ),
+          onTap: () {
+            setState(() {
+              counter++;
+            });
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'app_widget.dart';
+
+main() {
+  runApp(AppWidget());
+}
